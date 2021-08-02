8d99bfedb87482fe78a215a559bcd6a966e2dd77
Anand Shukla <anands2807@gmail.com>
2021-08-02 15:17:49 +0530

    blocking loop code example

--- server.js
+++ server.js
@@ -1,11 +1,22 @@
 const express = require('express');
 const app = express();
 
+function delay(duration){
+  const start = Date.now();
+  while(Date.now() - start < duration){
+    //event loop is blocked...
+  }
+};
+
 app.get('/', (req, res) => {
   res.send('Performance Example.')
 });
 
 app.get('/timer', (req, res) => {
   //delay the response:
+  delay(5000);
+
   res.send('DING DING...');
 });
+
+app.listen(3000);
